c86ef20b5b91b93926c52583db401e8f20709755
Alice <nameal1234@gmail.com>
2023-04-27 02:30:34 -0500

    edit mustache templates for board

--- js/board.js
+++ js/board.js
@@ -1,7 +1,9 @@
 $(document).ready(function() {
     $.getJSON("/js/board.json", function(data) {
         var output = Mustache.render($('#boardTemplate').html(), data);
+        var output2 = Mustache.render($('#modalTemplate').html(), data);
         $('#members').html(output);
+        $('#modals').html(output2);
     });
     
 });
