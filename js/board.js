d5a2e3a1f4fcf8c08d958e82be5b3a5d68e37eb9
Alice <nameal1234@gmail.com>
2023-05-05 12:45:19 -0500

    fixed modal and add image folders

--- js/board.js
+++ js/board.js
@@ -1,9 +1,13 @@
 $(document).ready(function() {
     $.getJSON("/js/board.json", function(data) {
-        var output = Mustache.render($('#boardTemplate').html(), data);
-        var output2 = Mustache.render($('#modalTemplate').html(), data);
-        $('#members').html(output);
-        $('#modals').html(output2);
+        // var output = Mustache.render($('#boardTemplate').html(), data);
+        // $('#members').html(output);
+        // var output2 = Mustache.render($('#modalTemplate').html(),data);
+        // $('#members').html += output2;
+        var size = Object.keys(data.values).length;
+        for(let i = 0; i < size; i++) {
+            var output = Mustache.render($("#boardTemplate").html(),data.values[i]);
+            $("#members").append(output);
+        }
     });
-    
 });
