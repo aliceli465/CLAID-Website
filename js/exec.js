16a061191b9d4afb7b736a6e814c044a51cce0a6
Alice <nameal1234@gmail.com>
2023-05-06 18:22:10 -0500

    added separate json file for board pics

--- js/exec.js
+++ js/exec.js
@@ -1,9 +1,9 @@
 $(document).ready(function() {
-    $.getJSON("/js/board.json", function(data) {
+    $.getJSON("/js/exec.json", function(data) {
         var size = Object.keys(data.values).length;
         for(let i = 0; i < size; i++) {
             var output = Mustache.render($("#boardTemplate").html(),data.values[i]);
-            $("#members").append(output);
+            $("#exec").append(output);
         }
     });
 });
