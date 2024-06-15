366438b2613e18422fcaa3feaa0ff5ae2032040c
Alice Li <nameal1234@gmail.com>
2024-06-14 23:03:25 -0700

    prettier

---
+++ src/components/NavBar.js
@@ -0,0 +1,38 @@
+import React from 'react';
+
+const NavBar = () => {
+  return (
+    <ul class="nav">
+      <li class="nav" style="margin-left:130px">
+        <a class="nav" href="/index.html">
+          Home
+        </a>
+      </li>
+      <li class="nav">
+        <a class="nav" href="about.html">
+          About Us
+        </a>
+      </li>
+      <li class="nav">
+        <a class="nav" href="board.html">
+          Board
+        </a>
+      </li>
+      <li class="nav">
+        <a class="nav" href="get_involved.html">
+          Get Involved
+        </a>
+      </li>
+      <li class="nav">
+        <a class="nav" href="connect.html">
+          Connect
+        </a>
+      </li>
+      <li class="nav">
+        <a class="nav" href="points.html">
+          Points
+        </a>
+      </li>
+    </ul>
+  );
+};
